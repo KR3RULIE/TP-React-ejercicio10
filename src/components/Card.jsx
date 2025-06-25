bc60050c552a4c923ebb18ed20076090e184bc89
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-06-25 10:21:31 -0300

    agregue 2 componentes que creo que me serviran

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import ItemCard from "./ItemCard";
+
+const Card = () => {
+  return (
+    <section className="container bg-warning my-4">
+      <h2 className="text-center">Peliculas guardadas</h2>
+      <ItemCard></ItemCard>
+    </section>
+  );
+};
+
+export default Card;
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,47 +1,51 @@
 import { Form, Button } from "react-bootstrap";
+import Card from "./Card";
 
 const Formulario = () => {
   return (
-    <section className="container bg-info">
-      <Form className="py-3">
-        <Form.Text className="text-light text-muted">
-          Los campos con un (<span className="text-danger">*</span>) son
-          obligatorios
-        </Form.Text>
+    <>
+      <section className="container bg-info my-4">
+        <Form className="py-3">
+          <Form.Text className="text-light text-muted">
+            Los campos con un (<span className="text-danger">*</span>) son
+            obligatorios
+          </Form.Text>
 
-        <Form.Group>
-          <Form.Label className="mt-2">
-            Nombre de la pelicula: <span className="text-danger">*</span>
-          </Form.Label>
-          <Form.Control type="text" placeholder="Matrix" />
-        </Form.Group>
+          <Form.Group>
+            <Form.Label className="mt-2">
+              Nombre de la pelicula: <span className="text-danger">*</span>
+            </Form.Label>
+            <Form.Control type="text" placeholder="Matrix" />
+          </Form.Group>
 
-        <Form.Group>
-          <Form.Label className="mt-2">
-            Descripcion de la pelicula: <span className="text-danger">*</span>
-          </Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Neo tiene que luchar para salvar a los refugiados,etc."
-          />
-        </Form.Group>
-         
-        <Form.Group>
-          <Form.Label className="mt-2">
-            Genero de la pelicula: <span className="text-danger">*</span>
-          </Form.Label>
-          <Form.Control type="text" placeholder="Ciencia ficción" />
-        </Form.Group>
+          <Form.Group>
+            <Form.Label className="mt-2">
+              Descripcion de la pelicula: <span className="text-danger">*</span>
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Neo tiene que luchar para salvar a los refugiados,etc."
+            />
+          </Form.Group>
 
-        <Button
-          variant="dark"
-          type="submit"
-          className="mt-3 d-flex mx-auto sombra2"
-        >
-          Agregar nueva pelicula
-        </Button>
-      </Form>
-    </section>
+          <Form.Group>
+            <Form.Label className="mt-2">
+              Genero de la pelicula: <span className="text-danger">*</span>
+            </Form.Label>
+            <Form.Control type="text" placeholder="Ciencia ficción" />
+          </Form.Group>
+
+          <Button
+            variant="dark"
+            type="submit"
+            className="mt-3 d-flex mx-auto sombra2"
+          >
+            Agregar nueva pelicula
+          </Button>
+        </Form>
+      </section>
+      <Card></Card>
+    </>
   );
 };
 
