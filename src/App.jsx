8c540190ceca07c9b1a571fad3a40cffaad81fcb
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-06-25 09:42:51 -0300

    baje el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
 
+function App() {
   return (
     <>
-      <h1>Ejercicio 10</h1>
+      <main>
+        <h1>Ejercicio 10</h1>
+      </main>
+      <footer className="text-center text-light bg-black py-1 sombra">
+        <p>
+          By{" "}
+          <a
+            href="https://github.com/KR3RULIE"
+            target="blank"
+            className="text-decoration-none text-danger"
+          >
+            KR3RULIE
+          </a>
+        </p>
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
